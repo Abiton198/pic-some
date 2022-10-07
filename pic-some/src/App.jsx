f267b01397d5e87d8082931d7860125b308bf9e2
abiton198 <abitonp@gmail.com>
2022-10-07 20:03:33 +0200

    .

---
+++ pic-some/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './Header'
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <h1>ABITON PADERA</h1>
+    </div>
+  )
+}
+
+export default App
